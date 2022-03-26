f204a5593f5c3eac3b01531c73658f5ed2cf6620
Diem Linh <Diem Linh@DESKTOP-08N4DNV>
2022-03-26 23:11:18 +0700

    Commit 21h ngay 26/3

--- 1911060839_Vuthidiemlinh_BigSchool/App_Start/BundleConfig.cs
+++ 1911060839_Vuthidiemlinh_BigSchool/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace _1911060839_Vuthidiemlinh_BigSchool
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
---
+++ 1911060839_Vuthidiemlinh_BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,56 @@
+﻿using _1911060839_Vuthidiemlinh_BigSchool.Models;
+using _1911060839_Vuthidiemlinh_BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1911060839_Vuthidiemlinh_BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModels)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModels.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModels);
+            }
+            var courses = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModels.GetDateTime(),
+                CategoryId = viewModels.Category,
+                Place = viewModels.Place,
+
+            };
+            _dbContext.Courses.Add(courses);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+
+
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories= _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+
+        
+    }
+}
